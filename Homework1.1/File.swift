bdd7766542a352e50fb5574b5cb7c702bd68231b
testgitIos12 <maksutovsamir635@gmail.com>
2022-01-06 21:54:11 +0600

    Initial Commit

---
+++ Homework1.1/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Homework1.1
+//
+//  Created by Samir Maksutov on 6/1/22.
+//
+
+import Foundation
---
+++ Homework1.1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Homework1.1
+//
+//  Created by Samir Maksutov on 6/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var Login: UITextField!
+    
+    @IBOutlet weak var Password: UITextField!
+    
+   
+    }
+
+
